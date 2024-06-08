dc6bd790c53b9361bc1c4600ec9bd4077d2b69f0
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2024-06-08 21:31:56 +0700

    Fix trigger signal

--- main.go
+++ main.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"errors"
-	"fmt"
 	"os"
 	"os/exec"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/rs/zerolog"
@@ -21,11 +22,6 @@ func main() {
 		log.Fatal().Err(err).Msg("start watcher")
 	}
 
-	cmd := exec.Command(os.Args[1], os.Args[2:]...)
-	cmd.Stdin = os.Stdin
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-
 	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
 		log.Fatal().Err(err).Msg("new watcher")
@@ -41,20 +37,39 @@ func main() {
 	timer := time.NewTimer(0)
 	defer timer.Stop()
 
-	go func() {
-		for range timer.C {
-			if cmd.Process != nil {
-				cmd.Process.Kill()
-				cmd.Process = nil
-			}
-			fmt.Println("start:", cmd)
-			if err := cmd.Start(); err != nil {
-				fmt.Println("start:", err)
-			}
-		}
-	}()
+	cmd := exec.Command(os.Args[1], os.Args[2:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
+
+	sig := make(chan os.Signal, 1)
+	defer close(sig)
+	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
+
 	for {
 		select {
+		case _, ok := <-timer.C:
+			if !ok {
+				return
+			}
+			kill(cmd)
+			if err := cmd.Start(); err != nil {
+				log.Err(err).Msg("start")
+			}
+			log.Debug().
+				Int("pid", cmd.Process.Pid).
+				Msg("start")
+		case s, ok := <-sig:
+			if !ok {
+				return
+			}
+			log.Debug().
+				Int("pid", cmd.Process.Pid).
+				Str("signal", s.String()).
+				Msg("signal")
+			kill(cmd)
+			return
 		case event, ok := <-watcher.Events:
 			if !ok {
 				return
@@ -74,3 +89,14 @@ func main() {
 }
 
 const debounce = 500 * time.Millisecond
+
+func kill(cmd *exec.Cmd) {
+	if cmd.Process != nil {
+		syscall.Kill(-cmd.Process.Pid, syscall.SIGTERM)
+		cmd.Process.Wait()
+		log.Debug().
+			Int("pid", cmd.Process.Pid).
+			Msg("killed")
+		cmd.Process = nil
+	}
+}
